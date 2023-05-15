17c6d0609330533ea70668fb8c0d4ff12108bbba
aadityannn <aaditya.nnn@gmail.com>
2023-05-15 23:49:21 +0530

    Update CSS

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -4,13 +4,13 @@ import { FaCheckCircle } from "react-icons/fa";
 function Thanks() {
     return (
         <div className="thanks-container">
-            <p className="thanks-message  animated-finite bounceIn">
+            <p className="thanks-message animated-fast bounceIn">
                 Thanks for subscribing!
             </p>
-            <p className="thanks-message  animated-finite bounceIn">
+            <p className="thanks-message animated-fast bounceIn">
                 You'll be notified when we launch our product.
             </p>
-            <FaCheckCircle className="thanks-icon  animated-infinite bounceIn" />
+            <FaCheckCircle className="thanks-icon animated-slow bounceIn" />
         </div>
     );
 }
